680342e4d464cfc20c4671fd39dbd4aa0ea58107
Richie <722461@student.dusd.net>
2024-11-01 10:00:16 -0700

    Added physics and health
    Added physics to game object so you can't walk right through them. And added health to ducko.

--- 2DBeginnerAdventureP2Quintero/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP2Quintero/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+   //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
+    }
 
-        Vector2 position = transform.position;
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime; ;
 
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
