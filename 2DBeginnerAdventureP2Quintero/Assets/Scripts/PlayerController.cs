ee48c0c7e8367466a767cb5077f113a141d4a423
Richie <722461@student.dusd.net>
2024-11-12 09:55:42 -0800

    Made enemy move
    Made enemy bot be able to move and make Ducko deal damage when he touches the bot.

--- 2DBeginnerAdventureP2Quintero/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP2Quintero/Assets/Scripts/PlayerController.cs
@@ -53,6 +53,15 @@ public class PlayerController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
